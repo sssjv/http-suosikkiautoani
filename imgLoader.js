7731f6e32fb501504b81f8a48c28aa28f173ada1
sssjv <50361505+sssjv@users.noreply.github.com>
2019-05-07 17:00:38 +0200

    Update imgLoader.js

--- imgLoader.js
+++ imgLoader.js
@@ -9,7 +9,12 @@ function cells(count) {
       images =['https://cdn.vox-cdn.com/thumbor/myT9EOeqv6-uH5Z3LYByziAvVfk=/0x0:2040x1360/1820x1213/filters:focal(857x517:1183x843):format(webp)/cdn.vox-cdn.com/uploads/chorus_image/image/61809461/akrales_171120_2110_0212.0.jpg',
 	       'https://techcrunch.com/wp-content/uploads/2018/06/L1160818-1.jpg?w=1390&crop=1', 
 	       'https://www.extremetech.com/wp-content/uploads/2019/03/250104_Volvo_Cars_to_deploy_in-car_cameras_and_intervention_against_intoxication-640x354.jpg', 
-	       'https://www.driving.co.uk/s3/st-driving-prod/uploads/2015/08/dvd-players-header.jpg'],
+	       'https://www.driving.co.uk/s3/st-driving-prod/uploads/2015/08/dvd-players-header.jpg',
+      
+      		'http://myufcw.org/files/2017/01/bluecars.jpg',
+		'https://www.sixt.co.uk/fileadmin/user_upload/ONM-3759_500x280_2-long-term-rental-discount.jpg',
+		'https://www.enterprise.ca/content/dam/ent-brand/replacement-rental-launchpad-review.jpg.wrend.480.270.jpeg',
+		'https://thenypost.files.wordpress.com/2019/01/car-rental.jpg?quality=90&strip=all&w=618&h=410&crop=1'],
       url,
       imageNum;
   
@@ -30,7 +35,6 @@ function cells(count) {
           '<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script><script>(adsbygoogle = window.adsbygoogle || []).push({google_ad_client: "ca-pub-1888985435305538",enable_page_level_ads: true});</script>'+
           '<a target="blank" href="'+ url +'"><img class="image--large" src="'+ url +'" alt="'+ url +'" /></a>' +
           '<p>Source : '+'<a target="blank" href="'+ url +'">'+url+'</a>'+'</p>'+
-          '<p>'+'adsense'+'</p>'+
       '</div>' +
 	  '</article>';
   }
